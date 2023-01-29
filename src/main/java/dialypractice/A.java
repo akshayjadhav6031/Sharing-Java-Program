156e4ae944802449fbd92dce092755604e7b6ca6
Akshay <akshayjadhav.6031@gmail.com>
2023-01-29 18:23:03 +0530

    new change

--- src/main/java/dialypractice/A.java
+++ src/main/java/dialypractice/A.java
@@ -14,7 +14,7 @@ public class A {
     public static void main(String[] args) {
         A a = new A();
         int two = 2;
-        int three = 3;
+        int three = 4;
         System.out.println("Multiplication Of ");
         System.out.println("2"+" "+"&"+" "+"3"+" "+"is");
         System.out.println(two*three);
